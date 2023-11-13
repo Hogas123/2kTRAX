7691a0c8a652bacf025f1626159e5a974741dc10
Logan Carrell <logancarrell48@gmail.com>
2023-11-12 21:33:15 -0500

    V 0.1.4 - game list functional - add button start setup

--- trax.py
+++ trax.py
@@ -66,43 +66,80 @@ class Game:
         return [self.player1, self.team1, self.score1, self.fgoal1, self.three1, self.player2, self.team2, self.score2, self.fgoal2, self.three2]
 
 class Label:
-    def __init__(self,m,x,y,w,h,data,type):
+    def __init__(self,data,type):
+        self.data = data
+        self.type = type
+
+    def draw(self):
+
+        if self.type == "Game":
+            out1 = "Player: " + str(self.data[0]) + " Team: " + str(self.data[1]) + " Score: " + str(self.data[2]) + " FG%: " + str(self.data[3]) + " 3P%: " + str(self.data[4]) 
+            out2 = "Player: " + str(self.data[5]) + " Team: " + str(self.data[6]) + " Score: " + str(self.data[7]) + " FG%: " + str(self.data[8]) + " 3P%: " + str(self.data[9]) 
+            text1 = m.displayText.render(out1, 0, "white")
+            text2 = m.displayText.render(out2, 0, "white")
+            return text1, text2
+            
+            
+        elif self.type == "Team":
+            out1 = "Player: " + str(self.data[0]) + " RECORD: (" + str(self.data[1]) + ", " + str(self.data[2]) + ")" 
+            out2 = "AVG Score: " + str(self.data[3]) + " FG%: " + str(self.data[4]) + " 3P%: " + str(self.data[5])
+            text1 = m.displayText.render(out1, 0, "white")
+            text2 = m.displayText.render(out2, 0, "white")
+            return text1, text2
+        
+        elif self.type == "Player":
+            out1 = "Team: " + str(self.data[0]) + " RECORD: (" + str(self.data[1]) + ", " + str(self.data[2]) + ")" 
+            out2 = "AVG Score: " + str(self.data[3]) + " FG%: " + str(self.data[4]) + " 3P%: " + str(self.data[5])
+            text1 = m.displayText.render(out1, 0, "white")
+            text2 = m.displayText.render(out2, 0, "white")
+            return text1, text2
+
+
+class ListDisp:
+    #height is of individual cell
+    def __init__(self, m, x, y, w, h, num, data):
         self.x = x
         self.y = y
         self.width = w
         self.height = h
         self.data = data
+        self.offset = 0
+        self.num = num
+        if self.num > 7: self.num = 7
 
-        pygame.draw.rect(m.view, "black", (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(m.view, "light grey", (self.x, self.y, self.width, self.num * self.height + self.height *0.5))
+        self.placeText()
+        
 
-        if type == "Game":
-            out1 = "Player: " + str(data[0]) + " Team: " + str(data[1]) + " Score: " + str(data[2]) + " FG%: " + str(data[3]) + " 3P%: " + str(data[4]) 
-            out2 = "Player: " + str(data[5]) + " Team: " + str(data[6]) + " Score: " + str(data[7]) + " FG%: " + str(data[8]) + " 3P%: " + str(data[9]) 
-            text1 = m.displayText.render(out1, 0, "white")
-            text2 = m.displayText.render(out2, 0, "white")
-            m.view.blit(text1, (self.x + self.width * 0.02, self.y))
-            m.view.blit(text2, (self.x + self.width * 0.02, self.y + self.height *0.42))
-            pygame.display.flip()
-            
-            
-        elif type == "Team":
-            out1 = "Player: " + str(data[0]) + " RECORD: (" + str(data[1]) + ", " + str(data[2]) + ")" 
-            out2 = "AVG Score: " + str(data[3]) + " FG%: " + str(data[4]) + " 3P%: " + str(data[5])
-            text1 = m.displayText.render(out1, 0, "white")
-            text2 = m.displayText.render(out2, 0, "white")
-            m.view.blit(text1, (self.x + self.width * 0.02, self.y))
-            m.view.blit(text2, (self.x + self.width * 0.02, self.y + self.height *0.42))
-            pygame.display.flip()
-        elif type == "Player":
-            out1 = "Team: " + str(data[0]) + " RECORD: (" + str(data[1]) + ", " + str(data[2]) + ")" 
-            out2 = "AVG Score: " + str(data[3]) + " FG%: " + str(data[4]) + " 3P%: " + str(data[5])
-            text1 = m.displayText.render(out1, 0, "white")
-            text2 = m.displayText.render(out2, 0, "white")
-            m.view.blit(text1, (self.x + self.width * 0.02, self.y))
-            m.view.blit(text2, (self.x + self.width * 0.02, self.y + self.height *0.42))
-            pygame.display.flip()
-
+        
+    
+    def placeText(self):
+        for i in range(self.num):
+            pygame.draw.rect(m.view, "black", (self.x + self.width *0.03, self.y+ self.height*0.2 + (i* self.height*1.04) , self.width * 0.94, self.height))
+        for i in range(self.num):
+            text = self.data[i + self.offset].draw()
+            m.view.blit(text[0], ((self.x + self.width *0.3, self.y+ self.height*0.3 + (i* self.height*1.04) , self.width * 0.94, self.height)))
+            m.view.blit(text[1], ((self.x + self.width *0.3, self.y+ self.height*0.7 + (i* self.height*1.04) , self.width * 0.94, self.height)))
+            text = m.displayText.render("GAME", 0, "white")
+            m.view.blit(text, ((self.x + self.width *0.05, self.y+ self.height*0.3 + (i* self.height*1.04) , self.width * 0.94, self.height)))
+            text = m.displayText.render(str(self.offset + i +1), 0, "white")
+            m.view.blit(text, ((self.x + self.width *0.05, self.y+ self.height*0.7 + (i* self.height*1.04) , self.width * 0.94, self.height)))
         pygame.display.flip()
+
+class Button:
+    def __init__(self,m,x,y,w,h,c,i):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.colour = c
+        self.img = pygame.image.load(i)
+
+        pygame.draw.rect(m.view, self.colour, (self.x, self.y, self.width, self.height))
+        self.img = pygame.transform.scale(self.img, (self.width * 0.94, self.height * 0.94))
+        m.view.blit(self.img, (self.x + self.width *0.03, self.y + self.height*0.03))
+        pygame.display.flip()
+
 
 class main:
     def __init__(self):
@@ -112,13 +149,15 @@ class main:
 
         pygame.display.init()
         pygame.font.init()
-        self.displayText = pygame.font.SysFont("Serif", 18)
+        self.displayText = pygame.font.SysFont("Serif", 20)
         disp_info = pygame.display.Info()
         pygame.display.set_caption("2KTRAX")
 
         self.view = pygame.display.set_mode((disp_info.current_w / 2.25, disp_info.current_h / 1.35))
         self.width = self.view.get_width()
         self.height = self.view.get_height()
+        self.curDisp = "Games"
+        
 
     def setBackground(self):
         baseC = 'dark grey'
@@ -126,7 +165,7 @@ class main:
         
         self.view.fill(baseC)
         self.button_p = pygame.draw.rect(self.view, panelC, (self.width * 0.02, self.height * 0.85, self.width * 0.96, self.height * 0.13 ))
-        pygame.display.flip()
+        self.add_btn = Button(m, m.width*0.45, m.height*0.85, m.width*0.15, m.height*0.13, 'red', './add.png')
 
     def initNBA(self):
 
@@ -246,13 +285,17 @@ class main:
                 self.gameList.append(Game(g_d[0], g_d[5], g_d[1], g_d[6], int(g_d[2]), int(g_d[7]), int(g_d[3]), int(g_d[8]), int(g_d[4]), int(g_d[9])))
             self.updateStats()
 
+    def displayGames(self):
+        data = []
+        for i in self.gameList:
+            data.append(Label(i.toLabelData(), "Game"))
+
+        self.gameDisp = ListDisp(m, m.width * 0.02, m.height *0.02, m.width*0.96, m.height*0.1, len(data), data)
 
 m = main()
 m.load()
 m.setBackground()
-
-l = Label(m, m.width * 0.05, m.height * 0.05, m.width * 0.9, m.height * 0.10 , ["Logan","Cavaliers",105,48,37,"Ben","Warriors",89,90,10], "Game" )
-
+m.displayGames()
 running = True
 
 while running:
@@ -263,6 +306,22 @@ while running:
         # QUIT app
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEWHEEL:
+            if m.curDisp == "Games":
+                if event.y == -1:
+                    if m.gameDisp.offset + m.gameDisp.num < len(m.gameDisp.data):
+                        m.gameDisp.offset -= event.y
+                        m.gameDisp.placeText()
+                else:
+                    if m.gameDisp.offset  > 0:
+                        m.gameDisp.offset -= event.y
+                        m.gameDisp.placeText()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            if pos[0] > m.add_btn.x and pos[0] < m.add_btn.x + m.add_btn.width and pos[1] > m.add_btn.y and pos[1] < m.add_btn.y + m.add_btn.height:
+                print('CLICK')
+
+
 
 
 
